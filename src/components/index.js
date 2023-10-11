c3572d6e8029c5bf77a12d7c2f0661b052326507
devKongkon <developerkongkon@gmail.com>
2023-10-11 11:32:52 +0600

    code updated

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Button from "./Button";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Logo from "./Logo";
+import PostCard from "./PostCard";
+import Select from "./Select";
+import Container from "./container/Container";
+
+
+
+export {
+    Input,
+    Select,
+    PostCard,
+    Button,
+    Header,
+    Footer,
+    Logo,
+    Container,
+    LogoutBtn
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authSlice from './authSlice';
 
 const store = configureStore({
     //I have to pass here root reducers
-    
+    reducer: {
+        auth: authSlice,
+    }
 })
 
 export default store
